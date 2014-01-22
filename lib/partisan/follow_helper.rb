3a4c9136911cd12b57201fbf2eb69019ae86ba3c
Jim <jdurand@o2web.ca>
2014-01-21 20:53:39 -0500

    Added support for Draper (or any model decorator implementation that responds to the 'object' method)

--- lib/partisan/follow_helper.rb
+++ lib/partisan/follow_helper.rb
@@ -5,7 +5,8 @@ module Partisan
 
     # Retrieves the parent class name if using STI.
     def parent_class_name(obj)
-      klass = obj.class
+      klass = obj.class if obj.class < ActiveRecord::Base
+      klass ||= obj.object.class if obj.respond_to?(:object) && obj.object.class < ActiveRecord::Base
       klass = klass.superclass while klass.superclass != ActiveRecord::Base
 
       klass.name
